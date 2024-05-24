ddcf654af0be39ef7a6c0529a00912e541698178
RJ Catalano <richard@thrackle.io>
2024-05-24 15:37:56 -0500

    testing suite
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- test/token/deployment/integration/BridgeTokenTest.t.sol
+++ test/token/deployment/integration/BridgeTokenTest.t.sol
@@ -8,6 +8,7 @@ import "src/token/Wave.sol";
 // note: needed to avoid conflict with ERC20 interface in OpenZeppelin
 import {InterchainTokenService} from "interchain-token-service/InterchainTokenService.sol";
 import {ITokenManagerType} from "interchain-token-service/interfaces/ITokenManagerType.sol";
+import {TokenManagerDeployer} from "interchain-token-service/utils/TokenManagerDeployer.sol";
 
 contract BridgeTokenTest is Test {
 
@@ -26,12 +27,30 @@ contract BridgeTokenTest is Test {
         wave = new Wave{salt: bytes32(0x534d454c4c494e475f53414c5453000000000000000000000000000000000000)}(ownerAddress, minterAddress);
         tokenService = InterchainTokenService(vm.envAddress("INTERCHAIN_TOKEN_SERVICE"));
 
+        // trying to set up with the ability to create logging
+
+        tokenService = new InterchainTokenService(
+            address(new TokenManagerDeployer()),
+            address(0x58667c5f134420Bf6904C7dD01fDDcB4Fea3a760),
+            address(0xe432150cce91c13a887f7D836923d5597adD8E31),
+            address(0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6),
+            address(0x83a93500d23Fbc3e82B410aD07A6a9F7A0670D66),
+            "ethereum-sepolia",
+            address(0x81a0545091864617E7037171FdfcBbdCFE3aeb23),
+            address(0x07715674F74c560200c7C95430673180812fCE73)
+        );
+        console.log("expected tokenID: ");
+        bytes32 expectedTokenId = tokenService.interchainTokenId(ownerAddress, bytes32(0x534d454c4c494e475f53414c5453000000000000000000000000000000000000));
+        console.logBytes32(expectedTokenId);
+        
+        console.log("expected token manager address: ", tokenService.tokenManagerAddress(expectedTokenId));
+
         bytes32 tokenId = tokenService.deployTokenManager(
             bytes32(0x534d454c4c494e475f53414c5453000000000000000000000000000000000000),
             "", 
             ITokenManagerType.TokenManagerType.LOCK_UNLOCK, 
             abi.encode(ownerAddress, address(wave)),
-            .01 ether
+            .009 ether
         );
 
         console.log("TOKEN_ID=");
