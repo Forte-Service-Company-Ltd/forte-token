cdf182d8204405bec41d7cab5f5ce0eb34dab3f6
TJ Everett <Taylereverett@gmail.com>
2025-04-07 07:49:48 -0700

    More updating submodules

--- test/token/deployment/integration/BridgeTokenTest.t.sol
+++ test/token/deployment/integration/BridgeTokenTest.t.sol
@@ -18,7 +18,7 @@ contract BridgeTokenTest is TestCommon {
     bytes32 tokenId;
     bytes32 salt;
 
-    ProtocolToken FOR;
+    ProtocolToken pToken;
     InterchainTokenService tokenService;
 
     function setUp() public {
